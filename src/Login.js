597a4fa75654e5f1123a62d4aea5ef365c71ef11
GitHuzda69 <hprmaa19@gmail.com>
2023-07-21 11:08:57 +0700

    improve structure and add react.js framework

---
+++ src/Login.js
@@ -0,0 +1,48 @@
+// src/Login.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/login', {
+        username,
+        password,
+      });
+      console.log(response.data); // You can handle authentication logic here
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- template/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-home = Blueprint('views', __name__)
-
-@home.route('/')
-def homescreen():
-    return "<h1>Test"
