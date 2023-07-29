6aa7dddd1e76af560cd8f877b15c5e760ad75b63
punya_huzda <99389482+GitHuzda69@users.noreply.github.com>
2023-07-29 14:03:50 +0700

    improve backend system

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,29 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express")
+const mysql = require("mysql")
+const cors = require("cors")
 
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 const db = mysql.createConnection({
-  host: 'localhost', // Ganti dengan host database MySQL Anda
-  user: 'username', // Ganti dengan username MySQL Anda
-  password: 'password', // Ganti dengan password MySQL Anda
-  database: 'database_name' // Ganti dengan nama database MySQL Anda
-});
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "huzda"
+})
 
-db.connect((err) => {
-  if (err) {
-    console.error('Koneksi ke database gagal: ', err);
-    return;
-  }
-  console.log('Terhubung ke database MySQL');
-});
+app.post('/signup', (req, res) =>  {
+    const sql = "INSERT INTO 'users' ('username','email','password') VALUES (?)";
+    const values = [req.body.username, req.body.email, req.body.password]
+    db.query(sql, [values], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
 
-// Tambahkan endpoint untuk login
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-  db.query(query, [username, password], (err, result) => {
-    if (err) {
-      console.error('Error saat login: ', err);
-      res.status(500).json({ message: 'Terjadi kesalahan saat login' });
-      return;
-    }
-
-    if (result.length === 1) {
-      res.status(200).json({ message: 'Login berhasil' });
-    } else {
-      res.status(401).json({ message: 'Login gagal' });
-    }
-  });
-});
-
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+app.listen(8081, ()=>{
+    console.log("listening....")
+})
