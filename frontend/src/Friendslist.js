64fef8f6bf7b6c4c95f6ea3ecbb9661fc8176566
YessaRidha <yessaridha@gmail.com>
2023-08-03 22:10:45 +0700

    Tambah dikit detail

--- frontend/src/Friendslist.js
+++ frontend/src/Friendslist.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
-function Friendlist() {
-    return (
-        <div>
-            TEMAN
-        </div>
-    ) 
+function Friendslist() {
+  return (
+    <div style={{ paddingLeft: "10px" }}>
+      <div>
+        <button>
+          <Link
+            to="/home"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Icon
+              icon="mingcute:back-2-fill"
+              width={29}
+              height={29}
+              color="black"
+            />
+          </Link>
+        </button>
+      </div>
+      Teman Konco
+    </div>
+  );
 }
 
-export default Friendlist
+export default Friendslist;
--- frontend/src/Setting.js
+++ frontend/src/Setting.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
-function Settings() {
-    return (
-    <div>
-        Settings
+function Setting() {
+  return (
+    <div style={{ paddingLeft: "10px" }}>
+      <div>
+        <button>
+          <Link
+            to="/home"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Icon
+              icon="mingcute:back-2-fill"
+              width={29}
+              height={29}
+              color="black"
+            />
+          </Link>
+        </button>
+      </div>
+      Settings
     </div>
-    )
+  );
 }
 
-export default Settings
+export default Setting;
