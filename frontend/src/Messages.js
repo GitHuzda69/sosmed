cf37b4fcf3ba4a697f326fee4c36a9c9cf922b12
punya_huzda <99389482+GitHuzda69@users.noreply.github.com>
2023-08-05 08:34:26 +0700

    fix conflict Messages.js

---
+++ frontend/src/Messages.js
@@ -0,0 +1,69 @@
+<<<<<<< yessa
+import React from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+
+function Messages() {
+  return (
+    <div style={{ paddingLeft: "10px" }}>
+      <div>
+        <button>
+          <Link
+            to="/home"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Icon
+              icon="mingcute:back-2-fill"
+              width={29}
+              height={29}
+              color="black"
+            />
+          </Link>
+        </button>
+      </div>
+      Messages
+    </div>
+  );
+}
+
+=======
+import React from 'react';
+import './Messages.css';
+
+const messages = [
+  {
+    sender: 'John Doe',
+    content: 'Hello there!',
+    timestamp: '2023-08-05 12:30',
+  },
+  {
+    sender: 'Jane Smith',
+    content: 'Hi John! How are you?',
+    timestamp: '2023-08-05 12:35',
+  },
+  // Add more messages here
+];
+
+const Messages = () => {
+  return (
+    <div className="messages-container">
+      <h1>Messages</h1>
+      <div className="message-list">
+        {messages.map((message, index) => (
+          <div className="message" key={index}>
+            <div className="message-sender">{message.sender}</div>
+            <div className="message-content">{message.content}</div>
+            <div className="message-timestamp">{message.timestamp}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+>>>>>>> main
+export default Messages;
