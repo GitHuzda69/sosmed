bef363abe54f26681dd3d51fcf57f61cd9a96caf
GitHuzda69 <hprmaa19@gmail.com>
2023-08-05 08:17:22 +0700

    add messages.js & messages.css

--- frontend/src/Messages.js
+++ frontend/src/Messages.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React from 'react';
+import './Messages.css';
 
-function Messages() {
-    return (
-        <div>
-            Messages
-        </div>
-    )
-}
+const messages = [
+  {
+    sender: 'John Doe',
+    content: 'Hello there!',
+    timestamp: '2023-08-05 12:30',
+  },
+  {
+    sender: 'Jane Smith',
+    content: 'Hi John! How are you?',
+    timestamp: '2023-08-05 12:35',
+  },
+  // Add more messages here
+];
 
-export default Messages
+const Messages = () => {
+  return (
+    <div className="messages-container">
+      <h1>Messages</h1>
+      <div className="message-list">
+        {messages.map((message, index) => (
+          <div className="message" key={index}>
+            <div className="message-sender">{message.sender}</div>
+            <div className="message-content">{message.content}</div>
+            <div className="message-timestamp">{message.timestamp}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Messages;
