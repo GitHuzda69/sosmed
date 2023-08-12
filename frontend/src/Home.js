70510ca722c6afd24777b9c89ca8cf03b4abe7e4
YessaRidha <yessaridha@gmail.com>
2023-08-12 23:21:01 +0700

    Edit comment

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -14,6 +14,7 @@ function Home() {
   const textareaRef = useRef(null);
   const [likeStatus, setLikeStatus] = useState({});
   const [selectedCommentPostId, setSelectedCommentPostId] = useState(null);
+  const [mode, setMode] = useState("post");
 
   const handleTextareaChange = () => {
     const textarea = textareaRef.current;
@@ -35,8 +36,10 @@ function Home() {
   const handleComment = (postId) => {
     if (selectedCommentPostId === postId) {
       setSelectedCommentPostId(null);
+      setMode("post"); // Switch back to "post" mode
     } else {
       setSelectedCommentPostId(postId);
+      setMode("comment"); // Switch to "comment" mode
     }
   };
 
@@ -105,27 +108,19 @@ function Home() {
       <Sidebar />
       <SearchBar onSearch={handleSearch} />
       <main className="main">
-        <div className="message-form">
+        <div className="content-form">
           <div className="button-container">
             <button className="button-content">
-              <Icon icon="oi:share" width={30} height={30} />
-            </button>
-            <button className="button-content">
-              <Icon icon="ci:download" width={30} height={30} />
-            </button>
-            <button className="button-content">
-              <Icon icon="mdi:heart-outline" width={30} height={30} />
-            </button>
-            <button className="button-content">
-              <Icon icon="ri:share-line" width={30} height={30} />
-            </button>
-            <button className="button-content">
-              <Icon
-                icon="radix-icons:dots-vertical"
-                rotate={1}
-                width={30}
-                height={30}
-              />
+              {selectedCommentPostId !== null && (
+                <button
+                  className="mode-switch-button"
+                  type="button"
+                  onClick={() => handleComment(selectedCommentPostId)}
+                >
+                  <Icon icon="akar-icons:arrow-back" />{" "}
+                  {mode === "comment" ? "Post" : "Comment"}
+                </button>
+              )}
             </button>
           </div>
           <form onSubmit={handleSubmit}>
@@ -160,96 +155,107 @@ function Home() {
           </form>
         </div>
         <div className="message-list-and-comments">
-          <div className="message-list">
-            {posts.map((post) => (
-              <div className="message" key={post.id}>
-                <div className="post-avatar">
-                  <img src={profilimage} alt="Avatar" />
-                </div>
-                <div className="post-container">
-                  <div className="post-content">
-                    <h2>{post.author}</h2>
-                    <p>{post.content}</p>
+          <div className="message-list-container">
+            <div className="message-list2">
+              {posts.map((post) => (
+                <div className="message-post" key={post.id}>
+                  <div className="post-avatar">
+                    <img src={profilimage} alt="Avatar" />
                   </div>
-                  <div className="button">
-                    <div className="likes-container">
-                      <div
-                        className={`likes ${
-                          likeStatus[post.id] ? "clicked" : ""
-                        }`}
-                      >
-                        <button onClick={() => handleLike(post.id)}>
-                          <Icon
-                            icon={
-                              likeStatus[post.id]
-                                ? "mdi:heart"
-                                : "mdi:heart-outline"
-                            }
-                            width={20}
-                            height={20}
-                            color={likeStatus[post.id] ? "red" : "black"}
-                            style={{
-                              marginLeft: "-5px",
-                              marginRight: "-5px",
-                            }}
-                          />
-                        </button>
-                        <span>{post.likes} Likes</span>
-                      </div>
-                      <div
-                        className="comment"
-                        onClick={() => handleComment(post.id)}
-                      >
-                        <button>
-                          <Icon
-                            icon="majesticons:comment-line"
-                            width={20}
-                            height={20}
-                            color="black"
-                            style={{
-                              marginLeft: "-5px",
-                              marginRight: "-5px",
-                            }}
-                          />
-                        </button>
-                        <span>Comments</span>
-                      </div>
-                      <div className="Shares">
-                        <button>
-                          <Icon
-                            icon="majesticons:share-line"
-                            width={22}
-                            height={22}
-                            color="black"
-                            style={{
-                              marginLeft: "-5px",
-                              marginRight: "-5px",
-                            }}
-                          />
-                        </button>
-                        <span>Shares</span>
+                  <div className="post-container">
+                    <div className="post-content">
+                      <h2>{post.author}</h2>
+                      <p>{post.content}</p>
+                    </div>
+                    <div className="button">
+                      <div className="likes-container">
+                        <div
+                          className={`likes ${
+                            likeStatus[post.id] ? "clicked" : ""
+                          }`}
+                        >
+                          <button onClick={() => handleLike(post.id)}>
+                            <Icon
+                              icon={
+                                likeStatus[post.id]
+                                  ? "mdi:heart"
+                                  : "mdi:heart-outline"
+                              }
+                              width={20}
+                              height={20}
+                              color={likeStatus[post.id] ? "red" : "black"}
+                              style={{
+                                marginLeft: "-5px",
+                                marginRight: "-5px",
+                              }}
+                            />
+                          </button>
+                          <span>{post.likes} Likes</span>
+                        </div>
+                        <div
+                          className="comment-button"
+                          onClick={() => handleComment(post.id)}
+                        >
+                          <button>
+                            <Icon
+                              icon="majesticons:comment-line"
+                              width={20}
+                              height={20}
+                              color="black"
+                              style={{
+                                marginLeft: "-5px",
+                                marginRight: "-5px",
+                              }}
+                            />
+                          </button>
+                          <span>Comments</span>
+                        </div>
+                        <div className="Shares">
+                          <button>
+                            <Icon
+                              icon="majesticons:share-line"
+                              width={22}
+                              height={22}
+                              color="black"
+                              style={{
+                                marginLeft: "-5px",
+                                marginRight: "-5px",
+                              }}
+                            />
+                          </button>
+                          <span>Shares</span>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-          <div className="comments-section">
-            {selectedCommentPostId !== null &&
-              posts.map((post) =>
-                post.id === selectedCommentPostId ? (
-                  <div key={post.id}>
-                    {post.comments &&
-                      post.comments.map((comment) => (
-                        <div className="comment" key={comment.id}>
-                          <h2>{comment.author}</h2>
-                          <p>{comment.content}</p>
-                        </div>
-                      ))}
-                  </div>
-                ) : null
-              )}
+          <div className="comments-section-container">
+            <div className="comments-section">
+              <h1>Comments</h1>
+              {selectedCommentPostId !== null &&
+                posts.map((post) =>
+                  post.id === selectedCommentPostId ? (
+                    <div key={post.id}>
+                      <h1>from {post.author} Post</h1>{" "}
+                      {post.comments &&
+                        post.comments.map((comment) => (
+                          <div className="comment" key={comment.id}>
+                            <div className="comment-avatar">
+                              <img src={profilimage} alt="Avatar-comment" />
+                            </div>
+                            <div className="comment-content">
+                              <h2>{comment.author}</h2>
+                              <p>{comment.content}</p>
+                            </div>
+                          </div>
+                        ))}
+                    </div>
+                  ) : null
+                )}
+            </div>
           </div>
         </div>
       </main>
