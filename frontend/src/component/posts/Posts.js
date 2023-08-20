5140ffddb6f3df9a79230b0ca12698fd3433b71a
YessaRidha <yessaridha@gmail.com>
2023-08-20 22:53:36 +0700

    EDIT post, posts, comment

--- frontend/src/component/posts/Posts.js
+++ frontend/src/component/posts/Posts.js
@@ -1,12 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
 import profilimage from "../../assets/profil.jpg";
+import friend3 from "../../assets/friend/friend3.jpg";
+import friend4 from "../../assets/friend/friend4.jpg";
 import { Icon } from "@iconify/react";
 import "./Posts.css";
 import Post from "../post/Post.js";
 
 const Posts = () => {
-
-  // Data Dummy
   const posts = [
     {
       id: 1,
@@ -14,7 +14,7 @@ const Posts = () => {
       userId: 1,
       profilePic: profilimage,
       desc: "Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd ",
-      img:"profilimage"
+      img: friend4,
     },
     {
       id: 2,
@@ -24,15 +24,31 @@ const Posts = () => {
       desc: "Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd asdkas a;askjhnd oiasdoiasoa isasdoi asoidh asoidihasoid ",
       img: "",
     },
+    {
+      id: 3,
+      name: "Orang",
+      userId: 3,
+      profilePic: profilimage,
+      desc: "Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd asdkas a;askjhnd oiasdoiasoa isasdoi asoidh asoidihasoid ",
+      img: "",
+    },
+    {
+      id: 4,
+      name: "Biasa",
+      userId: 4,
+      profilePic: profilimage,
+      desc: "Lorem ipsum dolor sit amet asioasdiLorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpods lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas os aosidjas asdpoasd po poadjpoasd asdkas a;askjhnd oiasdoiasoa isasdoi Lorem ipsum dolor sit amet asioasdiLorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpods lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas os aosidjas asdpoasd po poadjpoasd asdkas a;askjhnd oiasdoiasoa isas Lorem ipsum dolor sit amet asioasdiLorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpods lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas os aosidjas asdpoasd po poadjpoasd asdkas a;askjhnd oiasdoiasoa isas Lorem ipsum dolor sit amet asioasdiLorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpods lala asdkas Lorem ipsum dolor sit amet asioasdios aosidjas asdpoasd po poadjpoasd lala asdkas os aosidjas asdpoasd po poadjpoasd asdkas a;askjhnd oiasdoiasoa isasasoidh asoidihasoid ",
+      img: friend3,
+    },
   ];
 
-  return <div className="posts">
-      {posts.map(post=>(
+  return (
+    <div className="posts" style={{ width: "1200px" }}>
+      {posts.map((post) => (
         <Post post={post} key={[post.id]} />
       ))}
-      
     </div>
+  );
 };
-
 
 export default Posts;
