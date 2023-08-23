00329015fcd74202262f2bad76061e51c55bf91d
YessaRidha <yessaridha@gmail.com>
2023-08-23 21:03:11 +0700

    Edit leftbar & Rightbar

--- frontend/src/component/Leftbar/Leftbar.js
+++ frontend/src/component/Leftbar/Leftbar.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./Leftbar.css";
+import avatar from "../../assets/profil.jpg";
 import { Icon } from "@iconify/react";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <Link to='/'>
-      <button>
-        <Icon icon="ic:round-home" width="40" height="40" />
-      </button></Link>
+      <Link to="/">
+        <button>
+          <Icon icon="ic:round-home" width="40" height="40" />
+        </button>
+      </Link>
       <Link to="/settings">
-      <button>
-      <Icon icon="clarity:settings-solid" width="40" height="40" />
-      </button></Link>
+        <button>
+          <Icon icon="clarity:settings-solid" width="40" height="40" />
+        </button>
+      </Link>
       <button>
         <Icon icon="bi:instagram" width="40" height="40" />
       </button>
@@ -24,17 +27,31 @@ const Sidebar = () => {
         <Icon icon="mdi:twitter" width="40" height="40" />
       </button>
       <Link to="/messages">
-      <button>
-        <Icon icon="ant-design:message-filled" width="40" height="40" />
-      </button></Link>
+        <button>
+          <Icon icon="ant-design:message-filled" width="40" height="40" />
+        </button>
+      </Link>
       <Link to="/friend">
-      <button>
-        <Icon icon="fluent:people-community-20-filled" width="40" height="40" />
-      </button></Link>
-      <Link to='/login'>
-      <button>
-      <Icon icon="material-symbols:logout" width="40" height="40"/>
-      </button></Link>
+        <button>
+          <Icon
+            icon="fluent:people-community-20-filled"
+            width="40"
+            height="40"
+          />
+        </button>
+      </Link>
+      <Link to="/login">
+        <button>
+          <Icon icon="material-symbols:logout" width="40" height="40" />
+        </button>
+      </Link>
+      <Link
+        to={"/profile/${post.userId}"}
+        style={{ textDecoration: "none", color: "inherit" }}
+        className="profileavatar"
+      >
+        <img className="profile2" src={avatar} alt="" />
+      </Link>
     </div>
   );
 };
