9747754303f6d095ced678b194ef4a1e2a0bdfc3
GitHuzda69 <hprmaa19@gmail.com>
2023-09-25 11:24:54 +0700

    add friend endpoint

--- frontend/src/component/rightbar/Rightbar.js
+++ frontend/src/component/rightbar/Rightbar.js
@@ -1,106 +1,43 @@
 import "./Rightbar.css";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
-import avatar1 from "../../assets/friend/friend1.jpg";
-import avatar2 from "../../assets/friend/friend2.jpg";
-import avatar3 from "../../assets/friend/friend3.jpg";
-import avatar4 from "../../assets/friend/friend4.jpg";
-import avatar5 from "../../assets/friend/friend5.jpeg";
-import profilimage from "../../assets/profil.jpg";
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios.js";
 
 const Rightbar = () => {
-  const rightbar = [
-    {
-      id: 1,
-      name: "Jeou",
-      userId: 1,
-      profilePic: profilimage,
-      online: "Online ",
-      status: 1,
-    },
-    {
-      id: 2,
-      name: "Bukan",
-      userId: 2,
-      profilePic: avatar3,
-      online: "Online 44 minutes ago",
-      status: 0,
-    },
-    {
-      id: 3,
-      name: "Orang",
-      userId: 3,
-      profilePic: avatar4,
-      online: "Online 7 hours ago",
-      status: 0,
-    },
-    {
-      id: 4,
-      name: "Biasa",
-      userId: 4,
-      profilePic: avatar2,
-      online: "Online ",
-      status: 1,
-    },
-    {
-      id: 5,
-      name: "Lalo",
-      userId: 5,
-      profilePic: avatar1,
-      online: "Online 12 hours ago",
-      status: 0,
-    },
-    {
-      id: 6,
-      name: "Bila",
-      userId: 6,
-      profilePic: avatar5,
-      online: "Online ",
-      status: 1,
-    },
-    {
-      id: 7,
-      name: "Yoko",
-      userId: 7,
-      profilePic: avatar2,
-      online: "Online 2 days ago",
-      status: 0,
-    },
-    {
-      id: 8,
-      name: "Asas",
-      userId: 8,
-      profilePic: profilimage,
-      online: "Online ",
-      status: 1,
-    },
-  ];
+  const { isLoading, error ,data: friendData } = useQuery(["friend"], () =>
+    makeRequest.get("/friends").then((res) => {
+      return res.data;
+    })
+  );
   return (
     <div className="rightBar">
       <h2>Friends</h2>
       <div className="rightBarContainer">
-        {rightbar.map((friend) => (
-          <div key={friend.id} className="rightBarItem">
+        {error
+        ? `Something went wrong`
+        : isLoading
+        ? "loading" : friendData.map((friend) => (
+          <div key={friend.userid} className="rightBarItem">
             <div className="rightBarUser">
               <div className="rightBarUserInfo">
                 <img
                   className="rightBarImg"
-                  src={friend.profilePic}
-                  alt={friend.name}
+                  src={"./data/" + friend.profilepic}
+                  alt={friend.displayname}
                 />
                 <div
                   className={`statusDot ${
-                    friend.status === 1 ? "greenDot" : "grayDot"
+                    "grayDot"
                   }`}
                 />
               </div>
               <p
                 className={`rightBarUserStatus ${
-                  friend.status === 1 ? "rightBarUserStatus2" : ""
+                "grayDot"
                 }`}
               >
-                <span className="rightBarName">{friend.name}</span>
-                {friend.online}
+                <span className="rightBarName">{friend.displayname}</span>
               </p>
             </div>
             <Link to="/messages">
