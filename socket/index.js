bbd4541b81428be6eb70cde2ca5250923258fdea
GitHuzda69 <hprmaa19@gmail.com>
2023-11-19 15:03:44 +0700

    add socket getuser

--- socket/index.js
+++ socket/index.js
@@ -1,16 +1,28 @@
-// const io = require("socket.io")(8900, {
-//     cors: {
-//         origin: "http://localhost:3000"
-//     }
-// });
+const io = require("socket.io")(8900, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
 
-// io.on("connection", (socket) => {
-//     console.log("User connected");
-//     socket.on("disconnect", () => {
-//         console.log("User disconnected");
-//     });
+let users = []
 
-//     socket.on("error", (error) => {
-//         console.error("Socket error:", error);
-//     });
-// });
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId})
+}
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+    // take userId and sokcketId from User
+    socket.on("addUser", (userId, socketId) =>{
+        addUser(userId, socketId);
+        io.emit("getUsers", users)
+    })
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+
+    socket.on("error", (error) => {
+        console.error("Socket error:", error);
+    });
+});
