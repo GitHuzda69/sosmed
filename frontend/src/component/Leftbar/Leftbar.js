f46d532cdc86344ad36e2f62a7afb170b7199ef7
Yessa Keren <yessaridha@gmail.com>
2023-12-20 00:06:29 +0700

    edit upload + leftbar

--- frontend/src/component/Leftbar/Leftbar.js
+++ frontend/src/component/Leftbar/Leftbar.js
@@ -8,15 +8,9 @@ import Settings from "../Settings/Settings";
 import defaultprofile from "../../assets/profile/default_avatar.png";
 import logo from "../../assets/Logo_BNW.jpg";
 
-const Sidebar = ({
-  toggleSettings,
-  toggleLogout,
-  isFriendListPage,
-  isHomePage,
-  isMessagesPage,
-  isNotifPage,
-}) => {
+const Sidebar = ({ toggleSettings, toggleLogout, isFriendListPage, isHomePage, isMessagesPage, isNotifPage, isConnectPage, isProfilePage }) => {
   const { currentUser } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="sidebar">
@@ -25,67 +19,69 @@ const Sidebar = ({
         {isHomePage ? (
           <Link to="/">
             <button className="home-button">
-              <Icon
-                icon="mingcute:home-4-fill"
-                width="35"
-                height="35"
-                color="white"
-              />
+              <Icon icon="mingcute:home-4-fill" width="35" height="35" />
             </button>
           </Link>
         ) : (
           <Link to="/">
             <button className="home-button">
-              <Icon
-                icon="mingcute:home-4-line"
-                width="35"
-                height="35"
-                color="white"
-              />
+              <Icon icon="mingcute:home-4-line" width="35" height="35" />
             </button>
           </Link>
         )}
         {isNotifPage ? (
           <Link to="/notif">
             <button className="notif-button">
-              <Icon icon="mdi:bell" width="35" height="35" color="white" />
+              <Icon icon="mdi:bell" width="35" height="35" />
             </button>
           </Link>
         ) : (
           <Link to="/notif">
             <button className="notif-button">
-              <Icon
-                icon="mdi:bell-outline"
-                width="35"
-                height="35"
-                color="white"
-              />
+              <Icon icon="mdi:bell-outline" width="35" height="35" />
             </button>
           </Link>
         )}
         {isMessagesPage ? (
           <Link to="/messages">
             <button className="messages-button">
-              <Icon icon="bxs:chat" width="35" height="35" color="white" />
+              <Icon icon="bxs:chat" width="35" height="35" />
             </button>
           </Link>
         ) : (
           <Link to="/messages">
             <button className="messages-button">
-              <Icon icon="bx:chat" width="35" height="35" color="white" />
+              <Icon icon="bx:chat" width="35" height="35" />
             </button>
           </Link>
         )}
+        {isProfilePage ? (
+          <Link to={`/profile/${user.username}`}>
+            <button className="messages-button">
+              <Icon icon="iconamoon:profile-bold" width="35" height="35" />
+            </button>
+          </Link>
+        ) : (
+          <Link to={`/profile/${user.username}`}>
+            <button className="messages-button">
+              <Icon icon="iconamoon:profile-fill" width="35" height="35" />
+            </button>
+          </Link>
+        )}
+        {isConnectPage ? (
+          <button className="messages-button">
+            <Icon icon="carbon:link" width="35" height="35" />
+          </button>
+        ) : (
+          <button className="messages-button">
+            <Icon icon="carbon:link" width="35" height="35" />
+          </button>
+        )}
         <button onClick={toggleSettings}>
-          <Icon
-            icon="solar:settings-outline"
-            width="35"
-            height="35"
-            color="white"
-          />
+          <Icon icon="solar:settings-outline" width="35" height="35" />
         </button>
         <button onClick={toggleLogout}>
-          <Icon icon="tabler:logout" width="35" height="35" color="white" />
+          <Icon icon="tabler:logout" width="35" height="35" />
         </button>
       </div>
     </div>
