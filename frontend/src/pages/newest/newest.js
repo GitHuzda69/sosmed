64c78b9f1d475b5eb0027d9f0d95bf625b95a1b8
Yessa Keren <yessaridha@gmail.com>
2023-12-26 00:12:12 +0700

    edit ToS

--- frontend/src/pages/newest/newest.js
+++ frontend/src/pages/newest/newest.js
@@ -106,7 +106,7 @@ const Newest = (socket) => {
           <div className={`main-content ${!isShowRightBar ? "no-right-bar" : ""}`}>
             {!isShowRightBar && (
               <div className="newest-navbar">
-                <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} toggleLogout={toggleLogout} />
+                <Navbar isHomePage={isHomePage} isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} toggleLogout={toggleLogout} />
               </div>
             )}
             {isShowRightBar && (
